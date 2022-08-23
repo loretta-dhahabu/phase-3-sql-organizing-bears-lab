120f187682596dee68141925a03517ea35b2d782
loretta-dhahabu <loreydjefwa@gmail.com>
2022-08-23 20:10:19 +0300

    commit tree cbf562108d28017132c022ef84eeeca6bde864e7

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, sex, age, color, temperament, alive) VALUES ('Mr. Chocolate', 'M', 21, 'black', 'warm', 1);
+INSERT INTO bears (name, sex, age, color, temperament, alive) VALUES ('Rowdy', 'M', 5, 'black', 'hot tempered ', 0);
+INSERT INTO bears (name, sex, age, color, temperament, alive) VALUES ('Tabitha', 'F', 3, 'white', 'quick to react', 1);
+INSERT INTO bears (name, sex, age, color, temperament, alive) VALUES ('Sergeant Brown', 'M', 1, 'brown', 'pleasant', 1);
+INSERT INTO bears (name, sex, age, color, temperament, alive) VALUES ('Melissa','F', 2,  'black', 'slow-moving', 0);
+INSERT INTO bears (name, sex, age, color, temperament, alive) VALUES ('Grinch', 'M', 8, 'red', 'apathetic ', 1);
+INSERT INTO bears (name, sex, age, color, temperament, alive) VALUES ('Wendy', 'F', 11, 'red', 'depressed', 0);
+INSERT INTO bears (name, sex, age, color, temperament, alive) VALUES (NULL, 'M', 9, 'brown', 'sad', 1);
